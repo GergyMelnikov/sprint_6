336b9e84bb7ef54fda7c290ef75f3dfe603438a3
gmel <gmel003@icloud.com>
2024-06-09 17:31:03 +0300

    cleanup commented code

--- pages/header_page.py
+++ pages/header_page.py
@@ -17,18 +17,15 @@ class Logo(BasePage):
 
     @allure.step('Открыть главную страницу')
     def open_main(self):
-        #self.driver.get('https://qa-scooter.praktikum-services.ru/')
         self.open_url(self.BASE_URL)
 
 
     @allure.step('Нажать на логотип яндекса в хедере')
     def click_on_yandex_logo(self):
-        #self.wait.until(EC.element_to_be_clickable(self.yandex_logo)).click()
         self.click(self.yandex_logo)
 
 
     @allure.step('Нажать на логотип сервиса в хедере')
     def click_on_scotokat_logo(self):
-        #self.wait.until(EC.element_to_be_clickable(self.scotokat_logo)).click()
         self.click(self.scotokat_logo)
     
